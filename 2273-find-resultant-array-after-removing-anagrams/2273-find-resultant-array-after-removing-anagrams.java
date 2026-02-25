11e4071e917abe7fdbda6ee7923259400365e56f
6ankit <156463447+6ankit@users.noreply.github.com>
2026-02-25 17:12:30 +0530

    Time: 4 ms (24.82%), Space: 46.8 MB (23.13%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,24 +1,16 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        
-
         List<String> answer = new ArrayList<>();
         answer.add(words[0]);
         for(int i=1;i<words.length;i++){
             char[] ch = answer.get(answer.size()-1).toCharArray();
             char[] bh=words[i].toCharArray();
-
             Arrays.sort(ch);
             Arrays.sort(bh);
-
             String pb = new String(ch);
             String cb = new String(bh);
-
-            if(!pb.equals(cb)){
-                answer.add(words[i]);
-            }
+            if(!pb.equals(cb))  answer.add(words[i]);
         }
-
         return answer;
     }
 }
