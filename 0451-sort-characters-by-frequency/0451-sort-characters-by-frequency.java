d84b1e685b38efb8e4cfb4bca1f2f34625051cc4
6ankit <156463447+6ankit@users.noreply.github.com>
2026-02-25 17:27:50 +0530

    Time: 35 ms (10.57%), Space: 46.4 MB (89.24%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,42 @@
+class Pair{
+    private int id;
+    private char ch;
+
+    public Pair(int id,char ch){
+        this.id=id;
+        this.ch=ch;
+    }
+    public int getId() {return this.id;}
+    public char getChar(){return this.ch;}
+}
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character,Integer> freq = new HashMap<>();
+        for(char ch:s.toCharArray())
+        {
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
+            return b.getId()-a.getId();
+        });
+
+        for(Map.Entry<Character,Integer> m:freq.entrySet()){
+            Pair p = new Pair(m.getValue(),m.getKey());
+            pq.offer(p);
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        while(pq.size()>0){
+            Pair p=pq.peek();
+            int temp=p.getId();
+            while(temp>0) {
+                answer.append(p.getChar());
+                temp-=1;
+            }
+            pq.poll();
+        }
+        return answer.toString();
+    }
+}
