949c1ee2b60d71405f64cc53c9265c6ca4a16071
6ankit <156463447+6ankit@users.noreply.github.com>
2026-03-09 10:42:28 +0530

    Time: 3 ms (29.1%), Space: 43.2 MB (64.34%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,33 +1,30 @@
 class Solution {
-    String ans = "";
-
-    public void generateStrings(StringBuilder temp,Set<String> st,int size)
-    {
-        if(temp.length()>=size){
-            if(!st.contains(temp.toString()) && temp.length()==size)
-            {
-            ans=temp.toString();
+    int n;
+    Set<String> numsSet = new HashSet();
+    
+    private String generate(String curr) {
+        if (curr.length() == n) {
+            if (!numsSet.contains(curr)) {
+                return curr;
             }
-            return ;
+            
+            return "";
         }
         
-
-        temp.append('0');
-        generateStrings(temp,st,size);
-        temp.deleteCharAt(temp.length()-1);
-        temp.append('1');
-        generateStrings(temp,st,size);
-        temp.deleteCharAt(temp.length()-1);
-    }
-
-    public String findDifferentBinaryString(String[] nums) {
-        Set<String> st = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            st.add(nums[i]);
+        String addZero = generate(curr + "0");
+        if (addZero.length() > 0) {
+            return addZero;
         }
-
-        int size = nums[0].length();
-        generateStrings(new StringBuilder(), st, size);
-        return ans;
+        
+        return generate(curr + "1");
+    }
+    
+    public String findDifferentBinaryString(String[] nums) {
+        n = nums.length;
+        for (String s : nums) {
+            numsSet.add(s);
+        }
+        
+        return generate("");
     }
 }
