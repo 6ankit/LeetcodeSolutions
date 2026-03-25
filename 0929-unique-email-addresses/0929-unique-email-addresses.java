ee6ee81fbd04bb08a9eca81f9da8babe8fde590b
6ankit <156463447+6ankit@users.noreply.github.com>
2026-03-25 16:17:09 +0530

    Time: 7 ms (96.75%), Space: 46.1 MB (99.07%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,12 +1,12 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
+    // static {
+    //     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+    //         try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+    //             fw.write("0");
+    //         } catch (Exception e) {
+    //         }
+    //     }));
+    // }
     public int numUniqueEmails(String[] emails) {
         Set<String> set = new HashSet<>();
         int n = emails.length;
