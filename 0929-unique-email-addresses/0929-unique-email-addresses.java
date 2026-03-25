bb6092060f54989500bc3ecccf7fd4bf943fe595
6ankit <156463447+6ankit@users.noreply.github.com>
2026-03-25 16:17:49 +0530

    Time: 0 ms (100%), Space: 48.2 MB (6.85%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,43 +1,59 @@
 class Solution {
-    // static {
-    //     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-    //         try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-    //             fw.write("0");
-    //         } catch (Exception e) {
-    //         }
-    //     }));
-    // }
-    public int numUniqueEmails(String[] emails) {
-        Set<String> set = new HashSet<>();
-        int n = emails.length;
-        for(int i=0;i<n;i++){
-            String email = emails[i];
-            set.add(modifiedMail(email));
-        }
-        // System.out.print(set);
-        return set.size();
+      static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
     }
-    private String modifiedMail(String s){
-        StringBuilder sb = new StringBuilder();
-        boolean isPlus = false,isAt=false;
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='+') {
-                isPlus = true;
-                continue;
+    public int numUniqueEmails(String[] emails) {
+        for(int i=0;i<emails.length;i++){
+            char[] ch = emails[i].toCharArray();
+            int atTheRate=0;
+            String p="";
+            for(int j=0;j<ch.length;j++){
+                if(ch[j]=='.'){
+                    if(atTheRate==0){
+                        continue;
+                    }
+                    else {
+                        p+=ch[j];
+                        continue;
+                    }
+                }
+                if(ch[j]=='@') atTheRate+=1;
+                p+=ch[j];
             }
-            else if(c=='@'){
-                isAt = true;
-                sb.append(c);
-                continue;
+            if(p.length()>0) emails[i]=p;
+            int plus=0;
+            int index=0;
+            String x="";
+            p=emails[i];
+            int o=0;
+            int rate=0;
+            while(o<p.length()){
+                if(p.charAt(o)=='+' && rate==0){
+                    while(o<p.length() && rate==0){
+                        if(p.charAt(o)=='@'){
+                            x+='@';
+                            rate+=1;
+                        }
+                        o+=1;
+                    }
+                }
+                else{
+                    if(p.charAt(o)=='@')rate+=1;
+                    x+=p.charAt(o);
+                    o+=1;
+                }
             }
-            if(isAt){
-                sb.append(c);
-            }
-            else if(!isPlus && c != '.'){
-                sb.append(c);
-            }
+            if(x.length()>0) emails[i]=x;
         }
-        return sb.toString();
+        Map<String,Integer> mp=new HashMap<>();
+        for(int i=0;i<emails.length;i++){
+            mp.put(emails[i],mp.getOrDefault(emails[i],0)+1);
+        }
+        return mp.size();
     }
 }
