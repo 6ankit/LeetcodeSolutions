7fa970258daad1983563ff6c5f72d71a60ff50ce
6ankit <156463447+6ankit@users.noreply.github.com>
2026-04-14 22:59:55 +0530

    Time: 7 ms (9.28%), Space: 43.5 MB (48.1%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -7,7 +7,7 @@ class Solution {
         while(i<citations.length){
             for(int j=0;j<citations.length;j++){
                 if(citations[j]>=count){
-                    if(citations.length-j<count) continue;
+                    if(citations.length-j<count)return maxi;
                     maxi=count;
                     break;
                     // 0 1 3 5 6 
