0ba97e32b77cd42a4de230f32acf889394b826e7
6ankit <156463447+6ankit@users.noreply.github.com>
2026-04-14 22:59:04 +0530

    Time: 27 ms (5.04%), Space: 43.7 MB (19.25%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int maxi=0;
+        int count =1;
+        int i=0;
+        while(i<citations.length){
+            for(int j=0;j<citations.length;j++){
+                if(citations[j]>=count){
+                    if(citations.length-j<count) continue;
+                    maxi=count;
+                    break;
+                    // 0 1 3 5 6 
+                }
+            }
+            count+=1;
+            i+=1;
+        }
+        return maxi;
+    }
+}
