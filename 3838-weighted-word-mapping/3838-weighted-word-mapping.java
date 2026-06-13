044c34d08adbe9c29936dfc6ab311ee4c2f55554
6ankit <156463447+6ankit@users.noreply.github.com>
2026-06-13 09:29:37 +0530

    Time: 39 ms (5.58%), Space: 47.5 MB (6.44%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -7,15 +7,15 @@ class Solution {
             System.out.print(i+" ");
             reverse[26-(i+1)]=p.charAt(i);
         }
-        StringBuilder ans = new StringBuilder();
+        String ans ="";
         for(int i=0;i<words.length;i++){
             char[] ch = words[i].toCharArray();
             int w=0;
             for(char c:ch){
                 w+=weights[c-'a'];
             }
-            ans.append(reverse[w%26]);
+            ans+=reverse[w%26];
         }
-        return ans.toString();
+        return ans;
     }
 }
